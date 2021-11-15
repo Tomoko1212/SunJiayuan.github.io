e2d587a86bbe925cfa05181daf31749e984f17c2
sunjiayuan1212 <89896058+sunjiayuan1212@users.noreply.github.com>
2021-11-16 01:50:22 +0800

    Add files via upload

---
+++ homework3/3-4/Circle.php
@@ -0,0 +1,20 @@
+<?php
+     class Circle extends Shape{
+             private $radius=0;
+
+             function _construct(){
+                    $this->shapeName="圆形";
+                    if($this->validate($_POST['radius'],'圆的半径')){
+                        $this->radius.$_POST["radius"];
+                    }else{exit;}
+                    $this->radius=$_POST["radius"];
+             }
+             function area(){
+                     return pi()*$this->radius*$this->radius;
+             }
+             function perimeter(){
+                     return 2*pi()*$this->radius;
+             }
+     }
+
+?>
---
+++ homework3/3-4/index.php
@@ -0,0 +1,51 @@
+<html>
+
+   <head>
+
+      <title>图形计算器（面向对象）</title>
+
+      <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+   </head>
+
+   <body>
+
+        <center>
+
+             <h1>图形（面积 && 周长)计算器）<h1>
+
+             <a href="index.php?action=rect">矩形</a>||
+
+             <a href="index.php?action=triangle">三角形</a>||
+
+             <a href="index.php?action=circle">圆形</a>
+
+             <hr>
+
+        </center>
+
+        <?php
+
+               error_reporting(E_ALL & ~E_NOTICE);
+
+               function _autoload($className)
+
+               {
+
+                       include strtolower($className).".class.php";
+
+                     }
+
+                echo new Form();
+
+                if(isset($_POST["sub"])){
+
+                echo new Result();
+
+                  }
+
+         ?>
+
+  </body>
+
+</html>
